72cba3799550e509c97e68c6197075f0fdfdf11f
Ben Aderaw <asyncba@gmail.com>
2025-07-13 21:41:42 -0400

    feat: add BankOperations interface with abstract methods

---
+++ src/BankOperations.java
@@ -0,0 +1,11 @@
+interface BankOperations {
+    // user can
+    void createAccount();
+    void login();
+    void checkBalance();
+    void deposit();
+    void withdraw();
+    void checkTransactions();
+    void logout();
+    void closeAccount();
+}
